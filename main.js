f15392867326780221e3a8dfc6befddc6c285838
thibaut severac <thib3113@gmail.com>
2018-08-14 23:15:36 +0200

    work

--- main.js
+++ main.js
@@ -7,21 +7,42 @@ import TraefikCertificateExporter from "./lib/TraefikCertificateExporter";
 let argv = yargs
     .usage('usage: $0 <acmeJsonPath>')
     .command('[options] <acmeJsonPath>', 'export certificates from acme.json')
-    // .option('no-beep', {
-    //     describe: "don't beep after ping respond",
-    //     boolean: true,
-    //     default: false
-    // })
+    .option('domain', {
+        describe: "specify domain to export ( support blob, and comma separated string )",
+        alias: "d"
+    })
+    .option('output-folder', {
+        describe: "specify the folder where the certs are exported",
+        alias: "o"
+    })
+    .option('folder-structure', {
+        describe: "folder structure (1 = no folders, 2 = structured by main domain, 3 = 1+2)",
+        alias: "s",
+        default:TraefikCertificateExporter.FOLDERS_STRUCTURED
+    })
     .help('help')
     .alias("h", "help")
     .wrap(null)
     .argv;
 
 
-let acmeJsonPath = argv._[0];
+let acmeJsonPath = argv._[0] || ".";
+
+let domains = [];
+
+(argv.d||[]).forEach(domain => {
+   if(domain.match(/,/i)){
+       domain.split(",").forEach(d => domains.push(d));
+   }
+   else{
+       domains.push(domain);
+   }
+});
 
 let exporter = new TraefikCertificateExporter({
     acmeJsonPath,
+    structure:argv.s
 });
 
-exporter.export();
+// noinspection JSIgnoredPromiseFromCall
+exporter.export(domains);
